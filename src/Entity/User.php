5389c883437351603eb827f603148e390135d464
Steven Oyer <contact.stevenoyer@gmail.com>
2023-08-05 16:24:33 +0200

    Symfony recommandations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -147,7 +147,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        $this->password = null;
     }
 
     public function getName(): ?string
