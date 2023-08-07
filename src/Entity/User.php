ddb2d6c369266f426d89f5e581386d3e78ab0cca
Steven Oyer <contact.stevenoyer@gmail.com>
2023-08-07 08:38:00 +0200

    Fixes

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -151,7 +151,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
-        $this->password = null;
     }
 
     public function getName(): ?string
